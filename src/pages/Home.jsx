f3ec0cca09942766ddcb4955c38d60ec82c6ca6d
MukterHossain <mukterhossain3075@gmail.com>
2024-03-21 19:08:06 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Nav from "../component/Nav";
+
+const Home = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <h1>Home pge</h1>
+        </div>
+    );
+};
+
+export default Home;
